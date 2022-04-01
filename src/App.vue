45951d26458b8435cdaf2ad6aee4d44c3906f6b6
Joe O'Reilly <josephraoreilly@gmail.com>
2022-04-01 10:34:24 +0100

    added border color

--- src/App.vue
+++ src/App.vue
@@ -54,12 +54,12 @@
 
     <h2 class="mt-4 mb-8">More Responsive Forms</h2>
 
-  <div class="w-4/5 m-auto grid grid-cols-1 md:grid-cols-5 gap-4 mb-8 bg-slate-300 h-96 p-4 items-center">
+  <div class="w-4/5 m-auto grid grid-cols-1 md:grid-cols-5 gap-4 mb-8 bg-slate-50 h-auto p-8 items-center">
     <label for="name" class="col-span-2 h-12 flex items-center justify-center">Enter Name</label>
-    <input type="text" id="name" class="col-span-3 h-12 rounded-md">
+    <input type="text" id="name" class="col-span-3 h-12 rounded-md border border-gray-500">
 
     <label for="email" class="col-span-2 h-12 flex items-center justify-center">Enter Email</label>
-    <input type="text" id="email" class="col-span-3 h-12 rounded-md">
+    <input type="text" id="email" class="col-span-3 h-12 rounded-md border border-gray-500">
 
 
 
